dd6702e37019090bda5ec3641a9d6aeb83a9030c
Beno1tP <quangminhpham0602@gmail.com>
2024-12-23 10:44:38 +0700

    Update Labwork 2

--- student.mark.oop.py
+++ student.mark.oop.py
@@ -1,104 +1,119 @@
 class Student:
     def __init__(self, id, name, dob):
-        # __ for private, _ for protected
-        self.__id= id
-        self.__name= name
-        self.__dob= dob
+        self.__id = id
+        self.__name = name
+        self.__dob = dob
     
     def get_id(self):
         return self.__id
     def set_id(self, id):
-        self.__id= id
+        self.__id = id
 
-    
     def get_name(self):
         return self.__name
     def set_name(self, name):
-        self.__name= name
+        self.__name = name
     
     def get_dob(self):
         return self.__dob
     def set_dob(self, dob):
-        self.__dob= dob
+        self.__dob = dob
     
 
 class Course:
     def __init__(self, id, name):
-        self.__id= id
-        self.__name= name
+        self.__id = id
+        self.__name = name
     
     def get_id(self):
         return self.__id
     def set_id(self, id):
-        self.__id= id
+        self.__id = id
 
     def get_name(self):
         return self.__name
     def set_name(self, name):
-        self.__name= name
+        self.__name = name
         
 
 class StudentMark:
     def __init__(self, course_id, student_id, score):
-        self.__course_id= course_id
-        self.__student_id= student_id
-        self.__score= score
+        self.__course_id = course_id
+        self.__student_id = student_id
+        self.__score = score
 
     def get_course_id(self):
         return self.__course_id
     def set_course_id(self, course_id):
-        self.__course_id= course_id
+        self.__course_id = course_id
 
     def get_student_id(self):
         return self.__student_id
     def set_student_id(self, student_id):
-        self.__student_id= student_id
+        self.__student_id = student_id
 
     def get_score(self):
         return self.__score
     def set_score(self, score):
-        self.__score= score
+        self.__score = score
         
-def student_num():
-    return int(input("The number of students: "))
-    
-def course_num():
-    return int(input("The number of courses: "))
-    
-def list_course(courses):
+
+def input_students():
+    students = []
+    num_students = int(input("Enter the number of students: "))
+    for _ in range(num_students):
+        id = input("Student ID: ")
+        name = input("Student Name: ")
+        dob = input("Student Date of Birth (dd/mm/yyyy): ")
+        students.append(Student(id, name, dob))
+    return students
+
+
+def input_courses():
+    courses = []
+    num_courses = int(input("Enter the number of courses: "))
+    for _ in range(num_courses):
+        id = input("Course ID: ")
+        name = input("Course Name: ")
+        courses.append(Course(id, name))
+    return courses
+
+
+def input_marks(students, courses):
+    marks = []
     for course in courses:
-        print("Course ID: "+ str(course.get_id()))
-        print("Course name: "+ str(course.get_name()))
-        
-def list_student(students):
+        print(f"Entering marks for course: {course.get_name()} (ID: {course.get_id()})")
+        for student in students:
+            score = float(input(f"Enter marks for student {student.get_name()} (ID: {student.get_id()}): "))
+            marks.append(StudentMark(course.get_id(), student.get_id(), score))
+    return marks
+
+
+def list_courses(courses):
+    print("\nCourses:")
+    for course in courses:
+        print(f"ID: {course.get_id()}, Name: {course.get_name()}")
+
+
+def list_students(students):
+    print("\nStudents:")
     for student in students:
-        print("Student ID: "+ str(student.get_id()))
-        print("Student name: "+ str(student.get_name()))
-        print("DoB: "+ str(student.get_dob()))
-            
-def list_mark(course_id, marks):
-    print("Course ID: "+ course_id)
+        print(f"ID: {student.get_id()}, Name: {student.get_name()}, DoB: {student.get_dob()}")
+
+
+def list_marks(course_id, marks):
+    print(f"\nMarks for Course ID: {course_id}")
     for mark in marks:
-        if str(mark.get_course_id()) == course_id: 
-            print("Student ID: "+ str(mark.get_student_id()))
-            print("Mark: "+ str(mark.get_score()))
+        if mark.get_course_id() == course_id:
+            print(f"Student ID: {mark.get_student_id()}, Score: {mark.get_score()}")
 
-student_1 = Student("202412","Nigga","29/10/2004")
-student_2 = Student("202411", "Big", "07/03/2004")
-students = [student_1, student_2]
 
-course_1= Course("M2.001", "Maths")
-course_2= Course("M2.002", "Calculus")
-courses= [course_1, course_2]
+# Main program
+students = input_students()
+courses = input_courses()
+marks = input_marks(students, courses)
 
-mark_1= StudentMark("M2.001", "202412", 20)
-mark_2= StudentMark("M2.001", "202411", 20)
-mark_3= StudentMark("M2.002", "202412", 20)
-mark_4= StudentMark("M2.002", "202411", 20)
-marks= [mark_1, mark_2, mark_3, mark_4]
-
-student_num()
-course_num()
-list_course(courses)
-list_student(students)
-list_mark("M2.002", marks)
+list_courses(courses)
+list_students(students)
+course_id = input("\nEnter Course ID to view marks: ")
+list_marks(course_id, marks)
